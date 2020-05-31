a3cc852843719506ace4f4a4901295fb78cd562c
zhuNodeJs <1749238156@qq.com>
2020-05-31 19:11:47 +0800

    sh

--- webhook.js
+++ webhook.js
@@ -5,6 +5,15 @@ const handler = createHandler({
   secret: "myHashSecret"
 })
 
+function run_cmd(cmd, args, callback) {
+  var spawn = require('child_process').spawn;
+  var child = spawn(cmd, args);
+
+  var resp = '';
+  child.stdout.on('data', function(buffer) {resp += buffer.toString();})
+  child.stdout.on('end', function() {callback(resp);})
+}
+
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
     res.statusCode = 404
@@ -18,6 +27,14 @@ handler.on('error', err => {
   console.error('Error', err.message)
 })
 
-handler.on('*', event => {
-  console.log('Receive * ', event.payload)
+handler.on('push', event => {
+  console.log('Received a push event for %s to %s', 
+              event.payload.repository.name,
+              event.payload.ref);
+  if (event.payload.ref === 'refs/heads/master') {
+    console.log('deploy master....');
+    run_cmd('sh', ['./deploy-dev.sh'], function(text) {
+      console.log(text);
+    })
+  }
 })
